61d1425601172517d5ddaa89061c44ce1471f433
oswaldo <ozwaldo@gmail.com>
2022-08-04 15:36:23 -0500

    04

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,11 +3,36 @@ import { UsuarioContext } from '../context/UsuarioContext';
 
 export const Login = () => {
 
-    const {usuarioState} = useContext(UsuarioContext);
+    const {usuarioState, setUsuario} = useContext(UsuarioContext);
+   
+
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        let usuario = {
+            sobrenombre: e.target.sobrenombre.value,
+            nombre: e.target.nombre.value,
+            apellido: e.target.apellido.value
+        }
+
+        setUsuario(usuario);
+
+        
+    }
 
     return (
-        <div>
-            <p>Usuario: <strong>{usuarioState.sobrenombre}</strong></p>
-        </div>
+        <>
+            <div>
+                <p>Usuario: <strong>{usuarioState.sobrenombre}</strong></p>
+            </div>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <p><input type="text" placeholder='Usuario' name='sobrenombre'></input></p>
+                    <p><input type="text" placeholder='Nombre' name='nombre'></input></p>
+                    <p><input type="text" placeholder='Apellido' name='apellido'></input></p>
+                    <p><input type="submit" value="Enviar"></input></p>
+                </form>
+            </div>
+        </>
     )
 }
